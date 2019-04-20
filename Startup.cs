b516745e6941567a80fab7a5f66b535d3e8290ef
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-04-20 18:51:35 -0400

    Added the proper database connection.

--- Startup.cs
+++ Startup.cs
@@ -20,9 +20,9 @@ namespace SocialAppAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(o => o.UseSqlite("ConnectionString"));
+            services.AddDbContext<DataContext>(o => o.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }+
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
