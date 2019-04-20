89b4e5a9cbf80c66a2dadcac43d49ae6d3932734
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-04-20 18:07:56 -0400

    Added Sqlite to the project.

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SocialAppAPI.Data;
@@ -19,9 +20,9 @@ namespace SocialAppAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>();
+            services.AddDbContext<DataContext>(o => o.UseSqlite("ConnectionString"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+        }+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
