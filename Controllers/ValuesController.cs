7636219272beb6c93393b43bcc870a6fb59d38ac
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-04-20 19:49:34 -0400

    Added methods to grab things from the database.

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,5 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialAppAPI.Data;
+using SocialAppAPI.Models;
 
 namespace SocialAppAPI.Controllers
 {
@@ -9,18 +14,28 @@ namespace SocialAppAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValuesAsync()
         {
-            return new string[] { "value1", "value2" };
-        }
+            List<Value> values = await _context.Values.ToListAsync();
 
+            return Ok(values);
+        }
+        
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValueAsync(int id)
         {
-            return "value";
+            Value value = await _context.Values.FirstOrDefaultAsync(i => i.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
