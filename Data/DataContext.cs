1b9d53a07e2e606724b56ce36712925c7c41ed0d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-04-20 22:35:43 -0400

    Just trying to rearange things.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,8 +5,7 @@ namespace SocialAppAPI.Data
 {
     public class DataContext : DbContext
     {
-        
-        public DbSet<Value> Values { get; set; }
         public DataContext(DbContextOptions<DataContext> options):base(options) {}
+        public DbSet<Value> Values { get; set; }
     }
 }
